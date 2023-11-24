b2f7109942af8d7e43b03138d409bf0b55182466
kayki01 <kaykimacedo01@gmail.com>
2023-11-24 17:28:27 -0300

    atualização

--- js/cufon-replace.js
+++
@@ -1,4 +0,0 @@
-Cufon.replace('.corbel_bold_italic', { fontFamily: 'Corbel Bold Italic', hover: true }); 
-Cufon.replace('.corbel_italic', { fontFamily: 'Corbel Italic', hover: true }); 
-Cufon.replace('h4, h1, strong, .menu>li>a,.button-1, .menu2, #contato label, .enderecos ', { fontFamily: 'Corbel Bold', hover: true, textShadow: '-1px 1px rgba(0,0,0,0.1)'  }); 
-Cufon.replace('.corbel', { fontFamily: 'Corbel', hover: true }); 
